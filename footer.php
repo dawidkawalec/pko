594f8b3978ac748014682be4ec6d7e2053860359
Your Name <kontakt@dawidkawalec.pl>
2020-04-24 02:41:56 +0200

    header

--- footer.php
+++ footer.php
@@ -11,23 +11,24 @@
 
 ?>
 <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
-			</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- #content -->
-    <?php get_template_part( 'footer-widget' ); ?>
-	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
-		<div class="container pt-3 pb-3">
-            <div class="site-info">
-                &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
-                <span class="sep"> | </span>
-                <a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank" title="WordPress Technical Support" alt="Bootstrap WordPress Theme"><?php echo esc_html__('Bootstrap WordPress Theme','wp-bootstrap-starter'); ?></a>
 
-            </div><!-- close .site-info -->
-		</div>
-	</footer><!-- #colophon -->
+<?php get_template_part( 'footer-widget' ); ?>
+<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
+    <div class="container pt-3 pb-3">
+        <div class="site-info">
+            &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
+            <span class="sep"> | </span>
+            <a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank"
+                title="WordPress Technical Support"
+                alt="Bootstrap WordPress Theme"><?php echo esc_html__('Bootstrap WordPress Theme','wp-bootstrap-starter'); ?></a>
+
+        </div><!-- close .site-info -->
+    </div>
+</footer><!-- #colophon -->
 <?php endif; ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
