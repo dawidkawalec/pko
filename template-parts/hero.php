3a7ae9e8d5d6a1cc735ff93f078c6789333d8e03
Your Name <kontakt@dawidkawalec.pl>
2020-05-16 04:32:52 +0200

    home + wypadki

---
+++ template-parts/hero.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template part for hero
+ *
+ */
+
+?>
+
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <?php
+if ( function_exists('yoast_breadcrumb') ) {
+  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+}
+?>
+                <h1>Wypadek w pracy UK – odszkodowanie powypadkowe
+                    w Anglii za wypadek przy pracy</h1>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hero-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <p>Jeśli pracujesz w Wielkiej Brytanii i przydarzył Ci się wypadek w pracy, to zgłoś się do nas. Nasza
+                    kancelaria odszkodowań zajmuje się udzielaniem pomocy Polakom w ubieganiu się o odszkodowanie za
+                    wypadek w pracy w Anglii.</p>
+
+                <p>Możemy Ci pomóc, jeśli doznałeś obrażeń podczas wykonywania zadań na budowie, na magazynie, w
+                    fabryce, na farmie, a także w czasie jazdy na wózku widłowym. Obrażenia wynikające z powyższych
+                    sytuacji mogą być podstawą do uzyskania odpowiedniej rekompensaty. Wystarczy, że zgłosisz do nas
+                    swoją sprawę, a my zajmiemy się resztą. Dzięki temu, że jesteśmy polską kancelarią, nie musisz się o
+                    nic martwić. Zapewniamy wszystkie dokumenty w rodzimym języku, a formalności załatwiamy za Ciebie.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<section class="wypadek-content">
+    <div class="container">
+        <div class="row section-content">
+            <div class="col-lg-7 text-left">
+                <h2>Komu przysługuje odszkodowanie za wypadek w pracy w UK?</h2>
+                <p class="bold">Warto pamiętać, że w takim przypadku obcokrajowca obowiązują takie same prawa jak
+                    Anglika. Każda osoba poszkodowana w wyniku wypadku w pracy w UK ma prawo do należnej rekompensaty.
+                </p>
+
+                <p>
+                    Należy jednak pamiętać o dopełnieniu wszelkich formalności oraz dopilnowaniu terminu – osoba
+                    poszkodowana ma zazwyczaj 3 lata na zgłoszenie roszczeń i wszczęcie postępowania. Skontaktuj
+                    się z nami jeśli chciałbyś starać się odszkodowanie pod numerem: 0161 791 4669.</p>
+
+                <p>
+                    Rekompensata ta należy się zarówno w przypadku trwałej niepełnosprawności, jak i w przypadku
+                    tymczasowych bóli, a także strat finansowych. Należy przy tym zaznaczyć, że odszkodowania
+                    za wypadek w pracy w UK to nie przysługują osobom, które ponoszą odpowiedzialność za dane zdarzenie
+                    np
+                    gdy potknęliśmy się na prostej drodze lub upuściliśmy sobie cegłę na nogę. Zawsze rekomendujemy aby
+                    skorzystać z naszej porady prawnej, która pomoże ustalić odpowiedzialność
+                    na podstawie przepisów prawa. Warto pamiętać, że zdarzają się wypadki w której odpowiedzialność
+                    dzieli
+                    się proporcjonalnie np 75%/25%.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
