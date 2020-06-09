565627acbc691c65a7476a4efc121f3afbf83eaa
Your Name <kontakt@dawidkawalec.pl>
2020-06-09 17:33:53 +0200

    opinie + fixed header

---
+++ template/opinie.php
@@ -0,0 +1,318 @@
+<?php
+/**
+* Template Name: Opinie
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/hero-title');?>
+<div id="opinie">
+
+    <section class="home-o-nas">
+        <div class="container home-o-nas-wrapper">
+            <img src="https://dev.polskakancelariaodszkodowan.co.uk/wp-content/uploads/2020/04/sean-pollock-PhYq704ffdA-unsplash.jpg"
+                alt="">
+            <div class="row">
+                <div class="col-md-6">
+                </div>
+                <div class="col-md-6">
+                    <div class="home-o-nas-content">
+                        <p class="before-title">Więcej o nas</p>
+                        <h2>Polska Kancelaria Odszkodowań ™</h2>
+                        <div id="accordion-o-nas">
+                            <div id="pytanie1">
+                                <div id="headingOne">
+                                    <h3>
+                                        <a data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
+                                            aria-controls="collapseOne" class="collapse collapsed">
+                                            <span>KIM JESTEŚMY?</span>
+                                        </a>
+                                    </h3>
+                                </div>
+
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne"
+                                    data-parent="#accordion-o-nas" style="">
+                                    <p>
+                                        Polska Kancelaria Odszkodowań™ jest polsko – angielską kancelarią prawną
+                                        zajmującą
+                                        się uzyskiwaniem odszkodowania w UK, działającą na zasadzie NO WIN – NO FEE
+                                        z gwarancją przejrzystości kosztów. Posiadamy w sumie 65 lat doświadczenia, jako
+                                        prawnicy od odszkodowań. <a href="#">Zobacz najczęściej zadawane pytania o
+                                            odszkodowania w UK.</a>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                            <div id="pytanie2">
+                                <div id="heading2">
+                                    <h3>
+                                        <a data-toggle="collapse" data-target="#collapse2" aria-expanded="true"
+                                            aria-controls="collapse2" class="collapse collapsed">
+                                            <span>DLACZEGO MY?</span>
+                                        </a>
+                                    </h3>
+                                </div>
+
+                                <div id="collapse2" class="collapse" aria-labelledby="heading2"
+                                    data-parent="#accordion-o-nas">
+                                    <p>
+                                        Polska Kancelaria Odszkodowań™ jest polsko – angielską kancelarią prawną
+                                        zajmującą
+                                        się uzyskiwaniem odszkodowania w UK, działającą na zasadzie NO WIN – NO FEE
+                                        z gwarancją przejrzystości kosztów. Posiadamy w sumie 65 lat doświadczenia, jako
+                                        prawnicy od odszkodowań. <a href="#">Zobacz najczęściej zadawane pytania o
+                                            odszkodowania w UK.</a>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                            <div id="pytanie3">
+                                <div id="heading3">
+                                    <h3>
+                                        <a data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
+                                            aria-controls="collapse3" class="collapse collapsed">
+                                            <span>JESTEŚMY NOWOCZEŚNI TECHNOLOGICZNIE</span>
+                                        </a>
+                                    </h3>
+                                </div>
+
+                                <div id="collapse3" class="collapse" aria-labelledby="heading3"
+                                    data-parent="#accordion-o-nas">
+                                    <p>
+                                        Polska Kancelaria Odszkodowań™ jest polsko – angielską kancelarią prawną
+                                        zajmującą
+                                        się uzyskiwaniem odszkodowania w UK, działającą na zasadzie NO WIN – NO FEE
+                                        z gwarancją przejrzystości kosztów. Posiadamy w sumie 65 lat doświadczenia, jako
+                                        prawnicy od odszkodowań. <a href="#">Zobacz najczęściej zadawane pytania o
+                                            odszkodowania w UK.</a>
+                                    </p>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+                        <a href="#" class="btn-cta">Skontaktuj się z nami</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-referencje">
+        <div class="container">
+            <div class="row section-content">
+                <div class="col-md-12 text-center">
+                    <h2>Opinie naszych klientów</h2>
+                    <a class="under-title" href="#">Google Reviews – przeczytaj wszystkie opinie</a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 m-tb-30">
+                    <div class="referencja">
+                        <p class="ocena"><span>4.9</span>/5
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                            <img src="/wp-content/uploads/2020/05/gwiazdka.png" alt="">
+                        </p>
+                        <p class="referencja__name">Mariusz Ostaszewski</p>
+                        <a href="" class="referencja__link">Google Reviews – przeczytaj wszystkie opinie</a>
+                        <p class="referencja__opinia">
+                            Firma zdecydowanie godna polecenia. Zespół przyjazny i potrafi klarownie udzielić
+                            informacji
+                            na wszystkie zapytania. Jestem bardzo zadowolony z komunikacji oraz uzyskanego
+                            odszkodowania
+                            🙂
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-faq wypadki-loop white-bg team">
+        <div class="container">
+            <div class="row section-content">
+                <div class="col-md-12 text-center">
+                    <p class="before-title">Nasz team</p>
+                    <h2>Nasi eksperci</h2>
+                </div>
+
+                <div class="col-md-4 team-content">
+                    <h3>aaLorem ipsum dolor sit, amet consectetur adipisicing.</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, blanditiis possimus corporis
+                        exercitationem, doloribus odio fuga perspiciatis eos, accusantium officiis est? Ex corporis
+                        commodi cupiditate labore accusantium. Id, tempore dicta! s eos, accusantium officiis est? Ex
+                        corporis commodi cupid</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, blanditiis possimus corporis
+                        exercitationem, doloribus odio fuga perspiciatis eos, accusantium officiis est? Ex corporis
+                        commodi cupiditate labore accusantium. Id, tempore dicta!</p>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row">
+
+                        <div class="col-md-4">
+                            <div class="wypadek-loop">
+                                <a href="#">
+                                    <div class="wypadek-loop-img"
+                                        style="background:url('https://dev.polskakancelariaodszkodowan.co.uk/wp-content/uploads/2020/04/AdobeStock_246426078.jpg'); background-size:cover;border-top-right-radius: 1.5em; border-bottom-left-radius: 1.5em; height: 160px;">
+                                    </div>
+                                </a>
+                                <a href="#">
+                                    <h3>Odszkodowanie za wypadek w pracy UK</h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="wypadek-loop">
+                                <a href="#">
+                                    <div class="wypadek-loop-img"
+                                        style="background:url('https://dev.polskakancelariaodszkodowan.co.uk/wp-content/uploads/2020/04/AdobeStock_246426078.jpg'); background-size:cover;border-top-right-radius: 1.5em; border-bottom-left-radius: 1.5em; height: 160px;">
+                                    </div>
+                                </a>
+                                <a href="#">
+                                    <h3>Odszkodowanie za wypadek w pracy UK</h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="wypadek-loop">
+                                <a href="#">
+                                    <div class="wypadek-loop-img"
+                                        style="background:url('https://dev.polskakancelariaodszkodowan.co.uk/wp-content/uploads/2020/04/AdobeStock_246426078.jpg'); background-size:cover;border-top-right-radius: 1.5em; border-bottom-left-radius: 1.5em; height: 160px;">
+                                    </div>
+                                </a>
+                                <a href="#">
+                                    <h3>Odszkodowanie za wypadek w pracy UK</h3>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+</div>
+<?php get_template_part( 'template-parts/home-cta');?>
+
+<object style="width:100%;height: 350px; margin-bottom:-10px;"
+    data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2376.042418507366!2d-2.0807243844635734!3d53.4498279746565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bb5b295a9f0dd%3A0x6760071ba4257548!2sOdszkodowania%20UK%20-%20Polska%20Kancelaria%20Odszkodowa%C5%84!5e0!3m2!1spl!2spl!4v1588805991193!5m2!1spl!2spl, Polska&amp;ie=UTF8&amp;t=m&amp;z=16&amp;iwloc=B&amp;output=embed"
+    type="text/html"> </object>
+
+<?php
+get_footer();
